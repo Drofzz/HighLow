55123f0245e2671a82b3bc2cb11ff76a26a42748
Martin Fryd Wochner <martin.wochner@gmail.com>
2018-08-31 16:03:43 +0200

    Initial

---
+++ Game.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace HighLow
+{
+    class Game {
+        /// <summary>
+        /// The Number to guess
+        /// </summary>
+        public int Number { get; }
+
+        /// <summary>
+        /// Tries used to Guess
+        /// </summary>
+        public int Tries { get; private set; } = 0;
+
+        /// <summary>
+        /// Is Game Finish
+        /// </summary>
+        public bool Complete {get; private set;} = false;
+
+        /// <summary>
+        /// Create a new Game, from a Randomizer object, and the number between min and max
+        /// </summary>
+        /// <param name="random">Randomizer object</param>
+        /// <param name="min">Smallest Number</param>
+        /// <param name="max">Biggest Number</param>
+        public Game(Random random,int min, int max){
+            Number = random.Next(min,max);
+        }
+
+        /// <summary>
+        /// Make a guess
+        /// </summary>
+        /// <param name="guess">the number to guess</param>
+        /// <returns></returns>
+        public int Guess(int guess)
+        {
+            Tries++;
+            var percent = (float)guess / Number;
+
+            //If number is guessed
+            if (guess == Number)
+            {
+                Complete = true;
+                return 0;
+            }
+
+            if (percent >= 1.05f) return -2; //Grater then 5%
+            if (percent > 1.00f) return -1; //Grater then 0%
+            if (percent <= 0.95f) return 2; //Smaller then 5%
+            if (percent < 1.00f) return 1; //Smaller then 0%
+            throw new Exception("Invalid Range");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HighLow
+{
+    class Program
+    {
+        private static readonly Random Random = new Random();
+        static void Main(string[] args)
+        {
+            const int min = 0;
+            const int max = 1000;
+            var game = new Game(Random, min, max);
+            Console.WriteLine($"Try and guess the number between {min} and {max}");
+            Debug.WriteLine($"Pst! The number is {game.Number}");
+            while (!game.Complete)
+            {
+                var guess = int.Parse(Console.ReadLine());
+                var feedback = game.Guess(guess);
+
+                var adverb = "";
+                var adjective = "";
+
+                if (feedback == 0)
+                {
+                    Console.WriteLine("Success");
+                }
+                else
+                {
+                    if (Math.Abs(feedback) == 2) adverb = "lot";
+                    if (Math.Abs(feedback) == 1) adverb = "little";
+                    if (feedback > 0) adjective = "greater";
+                    if (feedback < 0) adjective = "less";
+
+                    Console.WriteLine($"Number is a {adverb} {adjective} than {guess}");
+                }
+            }
+            Console.WriteLine($"Number was {game.Number}");
+            Console.WriteLine("Game Over!");
+            Console.ReadKey();
+        }
+    }
+}
